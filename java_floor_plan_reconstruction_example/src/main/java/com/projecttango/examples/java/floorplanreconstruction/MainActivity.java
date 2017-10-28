47f90e67cc13bbcf182aa8df6109c73e6e1fd069
yyu233 <yalex778@163.com>
2017-10-28 13:37:45 -0500

    dougeshuo"bu dui a"

---
+++ java_floor_plan_reconstruction_example/src/main/java/com/projecttango/examples/java/floorplanreconstruction/MainActivity.java
@@ -0,0 +1,85 @@
+package com.projecttango.examples.java.floorplanreconstruction;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import static java.lang.Thread.sleep;
+
+public class MainActivity extends Activity {
+
+    private static int numOfClick = 0;
+    private static String startingPoint;
+    private static String destination;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (numOfClick < 2) {
+                    try {
+                        sleep(5);
+                    } catch (InterruptedException e) {
+                    }
+                }
+
+                Intent intent = new Intent(MainActivity.this,
+                        FloorPlanReconstructionActivity.class);
+                startActivity(intent);
+
+            }
+        }).start();
+
+
+    }
+
+    public void onFirstFloorClicked(View v) {
+        if (numOfClick == 0) {
+            startingPoint = "1";
+        } else {
+            destination = "1";
+        }
+        numOfClick++;
+    }
+
+    public void onSecondFloorClicked(View v) {
+        if (numOfClick == 0) {
+            startingPoint = "2";
+        } else {
+            destination = "2";
+        }
+        numOfClick++;
+    }
+
+    public void onThirdFloorClicked(View v) {
+        if (numOfClick == 0) {
+            startingPoint = "3";
+        } else {
+            destination = "3";
+        }
+        numOfClick++;
+    }
+
+    public void onFourthFloorClicked(View v) {
+        if (numOfClick == 0) {
+            startingPoint = "4";
+        } else {
+            destination = "4";
+        }
+        numOfClick++;
+    }
+
+    public static  String getStartingPoint() {
+        return startingPoint;
+    }
+
+    public static String getDestination() {
+        return destination;
+    }
+
+}
